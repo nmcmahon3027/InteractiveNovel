33b0598b99976a310f4c5c559e79d1790038f3a2
Neal McMahon <nmcmahon@luc.edu>
2019-02-08 21:24:37 -0600

    2/8/19: New stuff

--- src/SimpleJButton.java
+++ src/SimpleJButton.java
@@ -49,7 +49,7 @@ public class SimpleJButton {
             jTextField.setVisible(true);
             //jf.add(jTextField);
 
-            jf.pack();
+            jf.pack();//
             jf.setVisible(true);
             synchronized(button) {
                 try {
